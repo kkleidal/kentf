1c625f6514705a431235b1c0b5ef065da9b7f064
Ken Leidal <kkleidal@csail.mit.edu>
2017-09-10 12:24:30 -0400

    added code for splitting forward/backward passes over GPUs

---
+++ gpu.py
@@ -0,0 +1,7 @@
+import os
+
+def count_gpus():
+    devs = os.environ.get("CUDA_VISIBLE_DEVICES", "")
+    if len(devs) == 0:
+        return 0
+    return len(devs.split(",")) + 1
--- splitfn.py
+++ splitfn.py
@@ -2,6 +2,67 @@ import tensorflow as tf
 import tflearn as tfl
 from kentf.scoping import adapt_name
 
+def merge_grads(grad_list, name=None):
+    name = adapt_name(name, "merge-grads")
+    with tf.name_scope(name):
+        for i in range(len(grad_list)):
+            grad_list[i].sort(key=lambda x: x[1].name)
+        out = []
+        for i, (tensor, vari) in enumerate(grad_list[0]):
+            names = [grad_list[j][i][1].name for j in range(len(grad_list))]
+            assert all((name == vari.name for name in names)), names
+            grad_avg = tf.reduce_mean(
+                tf.stack([grad_list[j][i][0] for j in range(len(grad_list))], axis=0),
+                axis=0)
+            out.append((grad_avg, vari))
+        return out
+
+def split_and_recombined(inps, fn, num_splits, name=None):
+    name = adapt_name(name, "split-and-recombine")
+    with tf.name_scope(name):
+        adapted_inps = []
+        # Split inputs:
+        with tf.name_scope("preprocessing"):
+            for inp in inps:
+                if isinstance(inp, list) or isinstance(inp, tuple):
+                    if len(inp) % num_splits != 0:
+                        raise RuntimeError("List not divisible by number of splits: %s" % repr(inp))
+                    stride = len(inp) // num_splits
+                    squeeze = lambda x: x[0] if len(x) == 1 else x
+                    adapted_inps.append([squeeze(inp[i:(i+stride)]) for i in range(0, len(inp), stride)])
+                elif (isinstance(inp, tf.Variable) or isinstance(inp, tf.Tensor))\
+                        and len(inp.shape) > 0:
+                    if inp.shape[0].value is None:
+                        raise RuntimeError("Batch index must be defined for tensor")
+                    leng = int(inp.shape[0])
+                    if leng % num_splits != 0:
+                        raise RuntimeError("Tensor not divisible by number of splits (%d): %s" % (num_splits, inp.shape))
+                    stride = leng // num_splits
+                    adapted_inps.append([
+                        tf.slice(inp,
+                            [i if j == 0 else 0 for j in range(len(inp.shape))],
+                            [stride if j == 0 else -1 for j in range(len(inp.shape))])
+                        for i in range(0, leng, stride)])
+                else:
+                    adapted_inps.append([inp] * num_splits)
+        # Zip inputs to divide work:
+        adapted_inps = list(zip(*adapted_inps))
+        # Do work
+        raw_outputs = []
+        for split, args in enumerate(adapted_inps):
+            with tf.name_scope("bin%d" % split):
+                raw_outputs.append(fn(*args))
+        # Post-process outputs
+        outputs = []
+        with tf.name_scope("postprocessing"):
+            for i, group in enumerate(raw_outputs):
+                for j, var in enumerate(group):
+                    if i == 0:
+                        outputs.append([var])
+                    else:
+                        outputs[j].append(var)
+        return outputs
+
 # Useful for breaking up very large batch sizes to avoid allocating large tensors:
 def splitfn(inp, fn, maxbatch=None, allow_unrolling=True, name=None):
     name = adapt_name(name, "splitfn")
